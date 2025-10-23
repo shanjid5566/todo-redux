5a074b40006e2a343c86635f48dd954b7138104b
Shanjid Ahmed Rafi <shanjidahmed66@gmail.com>
2025-10-24 01:53:49 +0600

    add todos to local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit'
-import todoReducer from '../redux/features/todos/todoSlice'
+import todoReducer from './features/todos/todoSlice'
 
-export default configureStore({
-  reducer: {
-    // Add your reducers here
-    todos: todoReducer,
+// Load todos from localStorage
+const loadState = () => {
+  try {
+    const serialized = localStorage.getItem('todos');
+    if (serialized === null) return undefined;
+    return { todos: JSON.parse(serialized) };
+  } catch (e) {
+    console.error('Failed to load state from localStorage', e);
+    return undefined;
   }
-})
+}
+
+// Save todos to localStorage
+const saveState = (state) => {
+  try {
+    const serialized = JSON.stringify(state.todos);
+    localStorage.setItem('todos', serialized);
+  } catch (e) {
+    console.error('Failed to save state to localStorage', e);
+  }
+}
+
+const preloadedState = loadState();
+
+const store = configureStore({
+  reducer: {
+    todos: todoReducer,
+  },
+  preloadedState,
+});
+
+// Subscribe to store changes and persist todos
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+export default store;
